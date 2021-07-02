9e4c50305ff4536f58fa98a309a897b0cd275ddb
uzuriagawilliam <uzuriagawilliam@hotmail.com>
2021-07-02 13:15:07 +0200

    Övning 4

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -35,7 +37,7 @@ namespace SkalProj_Datastrukturer_Minne
                         ExamineList();
                         break;
                     case '2':
-                        ExamineQueue();
+                        ExamineQueue();//Jag blandar övinig ett och två i ExamineList()
                         break;
                     case '3':
                         ExamineStack();
@@ -62,28 +64,81 @@ namespace SkalProj_Datastrukturer_Minne
         /// </summary>
         static void ExamineList()
         {
-            /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch statement with cases '+' and '-'
-             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
-             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
-             * In both cases, look at the count and capacity of the list
-             * As a default case, tell them to use only + or -
-             * Below you can see some inspirational code to begin working.
-            */
+           //1. Stcken fungerar enligt FIFO prinsipen det vill säga first in first out.
+           //   I heapen står alla object i kön tillgengliga hela tiden
+           //2. Valee types är alla den som hör till System.ValueType
+           //   Reference Types hör till System.Object.
+           //   Reference Types lagras på heapen och lagras både på heapen och stacken
+           //3. MyInt x och MyInt y är av typen reference och pekar på en adress. Så när y.MyValue ändrar innehållet på adressen komer x att få den nya innehpllet som finns på  adressen. 
+            
+            Queue queue = new Queue();//Create an object av klassen Queue (Såg senare att det finns redan en fördefinerad Queue class)
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+           
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Please type you choose."
+                    + "\n'+' Before the name if you want tor add aperson fron the quewe"
+                    + "\n'-' if you want tor remove aperson fron the quewe"
+                    + "\n'0' to exit");
 
-            //switch(nav){...}
+                string input = " ";
+                try
+                {
+                    input = Console.ReadLine();//               
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                char nav = input[0];
+                string value = input.Substring(1);
+
+
+                switch (nav)
+                {
+                    case '+':
+                        {
+                            //theList.Add(value);//En element löggs i kön
+                            queue.QueueHandler('+', value);//En element löggs i kön
+                            break;
+                        }
+                    case '-':
+                        {
+                            //q.TestQueue('-', value, theList);
+                            queue.QueueHandler('-', value);//Första element i kön tas bort från kön
+                            break;
+                        }
+                    case '0':
+                        {
+                            Console.Clear();//Tillbaka till menyn
+                            flag = false;
+                            break;
+                        }
+
+                }
+
+                var capacity = queue.theList.Capacity;
+                Console.WriteLine($"Total capacity now {capacity} elements. {queue.theList.Count} Personer i kön: ");
+                foreach (string s in queue.theList)
+                {
+                    Console.WriteLine(s);
+                }
+
+
+                //F 2. Listan ökar efter den fjardet element läggs i listan
+                //F 3. Listan ökar med 4 elements till 8 och sedan till 16, 32, 64 ...
+                //F 4. Listan ökar inte i samma tack eftersson ökar med 4 elementer åt gången
+                //F 5. Listan minskar inte kapacitteten när elementer tas bort 
+                //F 6. Man kan använda en array när man vet exct hur många elemeter som behövs
+            }
         }
 
         /// <summary>
         /// Examines the datastructure Queue
         /// </summary>
-        static void ExamineQueue()
+        static void ExamineQueue()//Jag blandar övinig ett och två i ExamineList()
         {
             /*
              * Loop this method untill the user inputs something to exit to main menue.
@@ -102,18 +157,149 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+            
+            bool flag = true;
+            string userImput;
+            string tmpString; 
+            int count;
+            do
+            {
+                tmpString = "";
+                userImput = "";
+
+                Stack<char> stack = new Stack<char>();
+                
+                Console.WriteLine("Type in word to get reversed. 0 to exit");                
+                userImput = Console.ReadLine();
+
+                if (userImput == "0") flag = false;
+
+                count = userImput.Length;
+
+                for (int y = 0; y < userImput.Length; y++)
+                {
+                    stack.Push(userImput[y]);
+                }
+                foreach (var i in stack)
+                {
+                    tmpString += i ;
+                }
+                Console.WriteLine(tmpString);
+            } while (flag);
+            
         }
 
         static void CheckParanthesis()
         {
             /*
-             * Use this method to check if the paranthesis in a string is Correct or incorrect.
-             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
-             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             * Todo: Om man startar sökning med en en close parenthes " ),],}" spricker funktionen
              */
+            string userImput= "";
+            Console.WriteLine("Type a string to check for parentheses");
+            userImput = Console.ReadLine();
 
+            char[] testStr = { };
+ 
+
+            testStr = userImput.ToCharArray(0, userImput.Length);
+
+            List<char> list = new List<char>();
+             string[] tmpString= { };
+
+            for (int i = 0; i < testStr.Length; i++)
+			{
+
+                char t = testStr[i];
+
+                if( (t == ')' || t == ']' || t == '}') && (list.Count == 0))
+                {
+                    Console.WriteLine("Ike välformad sträng");
+                    Console.ReadKey();
+                    return;
+                }
+                
+
+                if ((t == '(') || (t == '[') || (t == '{'))
+                {
+                    //copy t till tmpStr
+                    list.Add(testStr[i]);
+                }
+
+                if (t == ')')
+                {
+                    //kolla i vilke pos "(" ligger. Om det ligger sist i tmpString  då tas den bort och programmet forsätter
+                    int l = 0;
+                    l = list.Count;
+                    if (list[l - 1] == '(')
+                    {
+                        list.Remove('(');
+                        continue;
+                    }
+                    else //
+                    {
+                        Console.WriteLine("Ike välformad sträng");
+                        Console.ReadKey();
+                        return;
+                    }
+
+                }
+                if (t == ']')
+                {
+                    //kolla i vilke pos "[" ligger. Om det ligger sist i tmpString  då tas den bort och programmet forsätter
+                    int l = 0;
+                    l = list.Count;
+                    if (list[l - 1] == '[')
+                    {
+                        list.Remove('[');
+                        continue;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Ike välformad sträng");
+                        Console.ReadKey();
+                        return;
+                    }
+
+                }
+                if (t == '}')
+                {
+                    //kolla i vilke pos "{" ligger. Om det ligger sist i tmpString då tas den bort och programmet forsätter
+                    int l = 0;
+                    l=list.Count;
+                    if (list[l-1] == '{')
+                    {
+                        list.Remove('{');
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ike välformad sträng");
+                        Console.ReadKey();
+                        return;
+                    }
+
+
+                }
+            }
+
+            if (list.Count != 0)
+            {
+                Console.WriteLine("Ike välformad sträng");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Välformad sträng");
+                Console.ReadKey();
+            }
         }
-
+   
+   
     }
+    
+
 }
+
+ 
+
 
---
+++ SkalProj_Datastrukturer_Minne/Queue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    public class Queue
+    {
+        public List<string> theList = new List<string>();
+        public void QueueHandler(char sig, string name)
+        {     
+            switch (sig)
+            {
+                case '+':
+                    {
+                        theList.Add(name);// En element löggs i kön                        
+                        break;
+                    }
+               case '-':            
+                    {
+                        int count = theList.Count;
+                        if(count == 0)//Om kön är tom hoppar vi ur switchen 
+                        {
+                            Console.WriteLine("The queue is empty");
+                            break;
+                        }
+                        theList.RemoveAt(0);//Första element tas bort från kön                    
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Wrong input");                        
+                        break;
+                    }
+            }
+            /*var capacity = theList.Capacity;
+            Console.WriteLine($"Total capacity now {capacity} elements. {theList.Count} Personer i kön: ");
+            foreach (string s in theList)
+            {
+                Console.WriteLine(s);
+            }*/
+
+        }
+    }
+}
